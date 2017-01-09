febbdb77d0f86b930bf144aa4dcbfadfda96ea90
Dung V. Pham <vietdzung@users.noreply.github.com>
2017-01-09 13:42:43 -0500

    created tile & symbol hierachy

--- asset/tile.js
+++ asset/tile.js
@@ -1,17 +1,23 @@
-Game.Tile = function(name, symbol) {
-  this.attr = {
-    _name: name,
-    _sym: symbol
-  };
+Game.Tile = function(properties) { //Pass in hash
+  if (!properties) {
+    properties = {};
+  }
+
+  if (!('attr' in this)) {
+    this.attr = {};
+  }
+
+  Game.Symbol.call(this, properties);
+
+  this.attr._name = properties.name || Game.UIMode.DEFAULT_BG
 };
+
+Game.Tile.extend(Game.Symbol);
 
 Game.Tile.prototype.getName = function() {
   return this.attr._name;
 };
-Game.Tile.prototype.getSymbol = function() {
-  return this.attr._sym;
-};
 
-Game.Tile.nullTile = new Game.Tile("nullTile", new Game.Symbol());
-Game.Tile.floorTile = new Game.Tile("floorTile", new Game.Symbol('.'));
-Game.Tile.wallTile = new Game.Tile("wallTile", new Game.Symbol('#'));
+Game.Tile.nullTile = new Game.Tile({name: "nullTile"});
+Game.Tile.floorTile = new Game.Tile({name: "floorTile", chr: '.'});
+Game.Tile.wallTile = new Game.Tile({name: "wallTile", chr: '#'});
